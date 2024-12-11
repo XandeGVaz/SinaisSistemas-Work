4429749beb6cc9be8f5031da42e526940b1a81e8
Xande <vitorgarciavaz@usp.br>
2024-12-11 18:36:17 -0300

    add integral function

--- final_work.py
+++ final_work.py
@@ -5,8 +5,26 @@
 # Vitor Alexandre Garcia Vaz - 14611432
 
 
-# imports
+# Imports
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from scipy.interpolate import interp1d
+
+
+# Integral de uma função
+def intg_s(a,b,N,f):
+    h=(b-a)/N ##calcula tamanho das partições
+    som=0 ##somatório da fórmula
+
+    x=np.zeros(N+1)
+    x[0]=a
+
+    for k in range(1,N+1):
+        x[k]=a+h*k ## valores de x a cada partição h
+    
+    for k in range (1,N+1):##cálculo do somatório
+        xb=(x[k-1]+x[k])/2
+        som=som+f(x[k-1])+4*f(xb)+f(x[k]) 
+
+    return ((h/6)*som)
