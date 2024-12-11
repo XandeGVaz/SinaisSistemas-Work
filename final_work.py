d39da0c6bd6b940d26a9c5e24dcba68e9b05d976
Xande <vitorgarciavaz@usp.br>
2024-12-11 18:25:51 -0300

    begin archives of final work

---
+++ final_work.py
@@ -0,0 +1,12 @@
+### Trabaho final de sinais e sistemas
+
+## Alunos:
+# Pedro Gasparelo Leme - 14602421
+# Vitor Alexandre Garcia Vaz - 14611432
+
+
+# imports
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy.interpolate import interp1d
